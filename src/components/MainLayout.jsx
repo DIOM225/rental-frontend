963cfb4a510be03a4868c912b490f6582fcd8099
diom16 <diom16_device@DIOM225s-MacBook-Pro.local>
2025-07-24 08:03:41 -0500

    Add Vercel Analytics to MainLayout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -2,12 +2,13 @@
 import React from 'react';
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
-
+import { Analytics } from '@vercel/analytics/react';
 function MainLayout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Analytics />
     </>
   );
 }
